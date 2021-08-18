3ccbeabe6f0d066176524de7463a39bf0caf1179
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-08-18 21:04:58 +0200

    feat: Initial commi. Basic Structure. Add component folder. Add Form.js NavBar.js Preview.js Select.js Step.js to components folder. Cleaned up create-react-app template unused code. Update READEME.md

---
+++ components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Custom made navbar component
+
+const NavBar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-brand"></div>
+            </nav>    
+        </>
+    )
+}
+
+export default NavBar
